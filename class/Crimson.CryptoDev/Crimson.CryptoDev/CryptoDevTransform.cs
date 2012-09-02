2051b9f0cbee709bd826ccddee42e092bfe9bb04
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2012-09-02 10:37:14 -0400

    Fix bottleneck (loops) in TransformFinalBlock when the buffers are larger than the block size. Issue found by Ajay Matai

--- class/Crimson.CryptoDev/Crimson.CryptoDev/CryptoDevTransform.cs
+++ class/Crimson.CryptoDev/Crimson.CryptoDev/CryptoDevTransform.cs
@@ -326,15 +326,10 @@ namespace Crimson.CryptoDev {
 			}
 
 			byte[] res = new byte [total];
-			int outputOffset = 0;
 
-			// process all blocks except the last (final) block
-			while (total > BlockSizeByte) {
-				InternalTransformBlock (inputBuffer, inputOffset, BlockSizeByte, res, outputOffset);
-				inputOffset += BlockSizeByte;
-				outputOffset += BlockSizeByte;
-				total -= BlockSizeByte;
-			}
+ 			// process all blocks except the last (final) block
+			int outputOffset = InternalTransformBlock (inputBuffer, inputOffset, total - BlockSizeByte, res, 0);
+			inputOffset += outputOffset;
 
 			// now we only have a single last block to encrypt
 			byte pad = (byte) (BlockSizeByte - rem);
@@ -379,14 +374,8 @@ namespace Crimson.CryptoDev {
 				total += BlockSizeByte;
 
 			byte[] res = new byte [total];
-			int outputOffset = 0;
 
-			while (inputCount > 0) {
-				int len = InternalTransformBlock (inputBuffer, inputOffset, BlockSizeByte, res, outputOffset);
-				inputOffset += BlockSizeByte;
-				outputOffset += len;
-				inputCount -= BlockSizeByte;
-			}
+			int outputOffset = InternalTransformBlock (inputBuffer, inputOffset, inputCount, res, 0);
 
 			if (lastBlock) {
 				Transform (workBuff, 0, res, outputOffset, BlockSizeByte);
