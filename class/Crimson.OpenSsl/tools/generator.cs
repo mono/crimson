13b5d51805a5d6626a80be400697a0943e1722f7
Bassam Tabbara <bassam@symform.com>
2013-09-25 23:59:42 -0700

    Remove unnecessary finalizer.

--- class/Crimson.OpenSsl/tools/generator.cs
+++ class/Crimson.OpenSsl/tools/generator.cs
@@ -110,13 +110,35 @@ namespace Crimson.Security.Cryptography {
 		File.WriteAllText (filename, content);
 	}
 
-	static void GenerateSymmetricAlgorithm (string name, string fallback, string ecb, string cbc)
+	static void GenerateSymmetricAlgorithm (string name, string fallback)
 	{
 		string template = @"// NOTE: Generated code DO NOT EDIT
 //
-// Author: Bassam Tabbara  <bassam@symform.com>
-// See LICENSE for copyrights and restrictions
+// Author: 
+//	Bassam Tabbara  <bassam@symform.com>
+// 
+// Copyright 2013 Symform Inc.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// 'Software'), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
+
 using System.Security.Cryptography;
 using Crimson.OpenSsl;
 
@@ -176,23 +198,18 @@ namespace Crimson.Security.Cryptography {
             }
         }
 
-        private Rijndael Fallback()
-        {
-            Rijndael r = new RijndaelManaged();
-            r.Mode = Mode;
-            r.Padding = Padding;
-            r.BlockSize = BlockSize;
-            return r;
-        }
+		private {1} Fallback ()
+		{
+			{1} r = new {1} ();
+			r.Mode = Mode;
+			r.Padding = Padding;
+			r.BlockSize = BlockSize;
+			return r;
+		}
 	}
 }";
-		string filename = Path.Combine (OutputDirectory,
-			String.Format ("{0}OpenSsl.g.cs", name));
-		string content = template.Replace ("{0}", name).
-			Replace ("{1}", fallback).
-			Replace ("{2}", ecb).
-			Replace ("{3}", cbc).
-			Replace ("{4}", name.ToUpperInvariant ());
+		string filename = Path.Combine (OutputDirectory, String.Format ("{0}OpenSsl.g.cs", name));
+		string content = template.Replace ("{0}", name).Replace ("{1}", fallback);
 		File.WriteAllText (filename, content);
 	}
 
@@ -212,8 +229,8 @@ namespace Crimson.Security.Cryptography {
 		GenerateHash ("SHA384");	// CRYPTO_SHA2_384
 		GenerateHash ("SHA512");	// CRYPTO_SHA2_512
 #endif
-		GenerateSymmetricAlgorithm ("Rijndael", "RijndaelManaged", "AES_ECB", "AES_CBC");
-		GenerateSymmetricAlgorithm ("Aes", "RijndaelManaged", "AES_ECB", "AES_CBC");
+		GenerateSymmetricAlgorithm ("Rijndael", "RijndaelManaged");
+		GenerateSymmetricAlgorithm ("Aes", "RijndaelManaged");
 #if UNTESTED
 		GenerateSymmetricAlgorithm ("Des", "DESCryptoServiceProvider", null, "DES_CBC");
 		GenerateSymmetricAlgorithm ("TripleDes", "TripleDESCryptoServiceProvider", null, "3DES_CBC");
